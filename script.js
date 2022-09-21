be6b6e5bd2dec6f89b2d16564d1117d248cc542f
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2022-09-21 16:12:28 +0200

    update

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-function expandIntro() {
-    let myElement = document.createElement('p'); // create variable
-    myElement.innerHTML = ('Extra intro text. <a href:"">Hey</a>'); // set content
-    document.getElementById('extraIntro').appendChild(myElement); // appending it to new element 'content'
-}
-
-let button = document.getElementById('extraIntro');
-button.addEventListener('click', expandIntro);
-
-
-
 // document.body.style.background = 'red'; // make the background red
 // setTimeout(() => document.body.style.background = '', 3000); // return back
-
-
-
 
 function changeColors() {
   document.body.style.background = 'var(--green)';
   document.getElementById('heead').style.backgroundColor = 'cyan';
-  document.getElementById('logo').style.transform = 'rotate(-180deg)';
+  // document.getElementById('logo').style.transform = 'rotate(-180deg)'; - This has been accomplished with CSS
 
 }
 
 function changeColorsBack() {
     document.body.style.background = 'cyan';
     document.getElementById('heead').style.backgroundColor = 'var(--pink)';
-    document.getElementById('logo').style.transform = 'rotate(0deg)';
+    // document.getElementById('logo').style.transform = 'rotate(0deg)'; - This has been accomplished with CSS
   }
 
 function colorDelay() {
@@ -38,3 +24,29 @@ function colorDelay() {
 logo.addEventListener('mouseover', changeColors);
 logo.addEventListener('mouseout', changeColorsBack);
 window.addEventListener('resize', colorDelay);
+
+
+// setTimeout(() => document.getElementById('logo').style.background = '', 1500)
+
+// function expandIntro() {
+//     let myElement = document.createElement('p'); // create variable
+//     myElement.innerHTML = ('Extra intro text. <a href:"">Hey</a>'); // set content
+//     document.getElementById('extraIntro').appendChild(myElement); // appending it to new element 'content'
+// }
+
+// let button = document.getElementById('extraIntro');
+// button.addEventListener('click', expandIntro);
+
+
+function clickBox () {
+    let readLess = document.getElementById('hide');
+    readLess.classList.add('hide');
+
+}
+
+let readLess = document.getElementById('lessIntro');
+readLess.addEventListener('click', clickBox);
+
+
+
+
