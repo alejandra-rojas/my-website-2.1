3743a59b2e77cd3ebea33a3e5c3acecabb9709ff
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2022-09-24 18:39:49 +0200

    modalupdate

--- script.js
+++ script.js
@@ -45,5 +45,34 @@ let readLess = document.getElementById('lessIntro');
 readLess.addEventListener('click', clickBox);
 
 
+// Modal 
+
+// Get modal element
+let modal = document.getElementById('simpleModal');
+// Get open modal button
+let modalBtn = document.getElementById('modalBtn');
+//Get close button
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click',openModal);
+//Listen for close click
+closeBtn.addEventListener('click',closeModal);
+//Listen for outside click
+window.addEventListener('click',outsideClick);
 
 
+//Function to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+//Function to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
+//Function to close modal if outside click
+function outsideClick(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
